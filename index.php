e8ca008321f4b79236214212217aeff918ddf3e1
Joe Gesualdo <joegesualdo@gmail.com>
2016-01-16 12:02:26 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+use WooWee\Brah;
+
+echo Brah::hi();
+echo "\n";
+echo Brah::yell();
+
+?>
---
+++ src/Brah.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace WooWee;
+
+class Brah{
+  public static function hi(){
+    echo "Hello";
+  }
+  public static function yell(){
+    echo "Ahhhhhhhh!";
+  }
+}
+?>
